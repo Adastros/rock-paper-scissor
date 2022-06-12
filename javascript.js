0a3fbaf3b12044e528b17f8ae45f8444a1f9337e
Danny <dannydo286@gmail.com>
2022-06-11 18:49:13 -0700

    Add initial function to play one round of the game

--- javascript.js
+++ javascript.js
@@ -12,3 +12,37 @@ function computerPlay () {
 
     return play;
 }
+
+/* plays a single round */
+function playRound (playerSelection, computerSelection) {
+    let lowerCasePlayerSelection = playerSelection.toLowerCase();
+    let lowerCaseComputerSelection = computerSelection.toLowerCase();
+    let results = '';
+
+    console.log("computer: " + lowerCaseComputerSelection);
+    console.log("Player: " + lowerCasePlayerSelection);
+
+    if (lowerCasePlayerSelection === lowerCaseComputerSelection) {
+        results = "It's a tie!";
+    } else if (lowerCasePlayerSelection === 'rock'){
+        if (lowerCaseComputerSelection === 'paper') {
+            results = "You lose! Paper beats Rock"
+        } else {
+            results = "You win! Rock beats Scissors"
+        }
+    } else if (lowerCasePlayerSelection === 'paper') {
+        if (lowerCaseComputerSelection === 'scissors') {
+            results = "You lose! Scissors beats Paper"
+        } else {
+            results = "You win! Paper beats Rock"
+        }
+    } else if (lowerCasePlayerSelection === 'scissors') {
+        if (lowerCaseComputerSelection === 'rock') {
+            results = "You lose! Rock beats Scissors"
+        } else {
+            results = "You win! Scissors beats Paper"
+        }
+    }
+
+    return results;
+}
