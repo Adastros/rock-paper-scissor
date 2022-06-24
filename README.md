6c0d127fd4bfbee22efcb57673370f711af24110
Danny <dannydo286@gmail.com>
2022-06-23 23:39:30 -0700

    Update README and refactor computerPlay
    README was updated to log my study progress. Function computerPlay was
    updated to primarily improve efficiency. It's faster to choose an
    element in an array than go through multiple conditions.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This is a browser-based game of rock, paper, or scissors. 
 
 This project is part of the curriculum for The Odin Project.
+
+What I learned or improved on:
+- how to target and dynamically interact with nodes
+- how to create a branch of my main repo and merge the branch back to
+the main branch
+- how to better structure my code for readability and organization
+- the README.md file can be marked up
+- whiteboard-ing and pseudo coding the problems I faced with this project
--- javascript.js
+++ javascript.js
@@ -1,24 +1,34 @@
+let computerScore = 0,
+    playerScore = 0;
+
+let playerScoreNode = document.getElementById('playerScore'),
+    computerScoreNode = document.getElementById('computerScore'),
+    resultNode = document.getElementById('textResult');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    return button.addEventListener('click', (e) => {
+        let roundResult = playRound(e.target.textContent, computerPlay());
+        updateScore(roundResult);
+        displayPlayerScore(playerScore);
+        displayComputerScore(computerScore);
+        displayTextResult(roundResult);
+    });
+});
+
 function computerPlay () {
-    let randomNum = Math.floor(Math.random() * 3);
-    let play;
+    let randomNum = Math.floor(Math.random() * 3),
+        play = ['rock', 'paper', 'scissors'];
 
-    if (randomNum === 0) {
-        play = 'rock';
-    } else if (randomNum === 1) {
-        play = 'paper';
-    } else {
-        play = 'scissors';
-    }
-
-    return play;
+    return play[randomNum];
 }
 
 function playRound (playerSelection, computerSelection) {
-    let lowerCasePlayerSelection = playerSelection.toLowerCase();
-    let result = '';
+    let lowerCasePlayerSelection = playerSelection.toLowerCase(),
+        result = '';
 
-    console.log("computer: " + computerSelection);
-    console.log("Player: " + lowerCasePlayerSelection);
+    //console.log("computer: " + computerSelection);
+    //console.log("Player: " + lowerCasePlayerSelection);
 
     if (lowerCasePlayerSelection === computerSelection) {
         result = "It's a tie!";
@@ -54,18 +64,15 @@ function updateScore (roundResult) {
 }
 
 function displayPlayerScore (playerScore) {
-    let playerScoreNode = document.getElementById('playerScore');
     playerScoreNode.textContent = playerScore;
 }
 
 function displayComputerScore (computerScore) {
-    let computerScoreNode = document.getElementById('computerScore');
     computerScoreNode.textContent = computerScore;
 }
 
 function displayTextResult (roundResult) {
-    let resultNode = document.getElementById('textResult'),
-        gameResult = '';
+    let gameResult = '';
 
     if (computerScore === 5) {
         computerScore = 0;
@@ -79,18 +86,3 @@ function displayTextResult (roundResult) {
 
     resultNode.textContent = roundResult + gameResult;
 }
-
-let computerScore = 0,
-    playerScore = 0;
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    return button.addEventListener('click', (e) => {
-        let roundResult = playRound(e.target.textContent, computerPlay());
-        updateScore(roundResult);
-        displayPlayerScore(playerScore);
-        displayComputerScore(computerScore);
-        displayTextResult(roundResult);
-    });
-});
-
